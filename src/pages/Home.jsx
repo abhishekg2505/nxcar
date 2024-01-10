5e28a6be007c81516055ae90e1ff110a0587d5cb
abhishekg2505 <abhishekg2505@gmail.com>
2024-01-10 18:16:01 +0530

    add animation on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const [number, setNumber] = useState(false)
+
+  const changeNumber = ()=>{
+    setNumber(!number)
+  }
   return (
     <div className='home-section'>
-      <h1>Take A SPIN</h1>
-      <button>Get Started</button>
+      <div class="absNumber">{number ? "02" : "01"}</div>
+      <h2 style={{ opacity: number ? 1 : 0 }}>Take A SPIN</h2>
+      <h1 style={{ opacity: number ? 0 : 1 }}>Take A SPIN</h1>
+      <button onClick={changeNumber}>Get Started</button>
     </div>
   )
 }
